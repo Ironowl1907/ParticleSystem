8ec6e7fd91be8d67ffd39764fe1d78ff112387a0
Ironowl <facuguialtana7@gmail.com>
2024-07-26 08:31:07 -0300

    Basic Definitions

---
+++ build/CMakeFiles/Particles.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Particles.dir/main.cpp.o"
+  "CMakeFiles/Particles.dir/main.cpp.o.d"
+  "Particles"
+  "Particles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Particles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
